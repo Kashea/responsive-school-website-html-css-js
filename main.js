a33ae9396ffd74a7e86def804f50cb0ab22e9f75
Rossrival Moises Bendillo <116779772+Kashea@users.noreply.github.com>
2023-07-05 17:53:02 +0800

    homepage completed

--- main.js
+++ main.js
@@ -4,3 +4,32 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
     ('window-scroll', window.scrollY > 0)
 })
+
+// show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        //change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus') {
+            icon.className = "uil uil-minus"
+        } else {
+            icon.className = "uil uil-plus"
+        }
+    })
+})
+
+//swiper
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween:30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable:true,
+    },
+});
